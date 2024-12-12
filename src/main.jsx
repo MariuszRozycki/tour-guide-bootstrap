7ca110179c96542f5acfcd3c27b0bde4b7c3f3e2
MariuszRozycki <mariansc83@icloud.com>
2024-12-12 14:38:39 +0100

    Correct import bootstrap files

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/main.scss";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SearchProvider } from "./context/SearchContext.jsx";
 import { FavoritesProvider } from "./context/FavoritesContext.jsx";
+// import "./styles/main.scss";
+import "../node_modules/swiper/swiper-bundle.min.css";
 
 const queryClient = new QueryClient();
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @import "bootstrap/scss/functions";
-          @import "bootstrap/scss/mixins";
-          @import "bootstrap/scss/variables";
+          @import "src/styles/main.scss";
         `,
       },
     },
