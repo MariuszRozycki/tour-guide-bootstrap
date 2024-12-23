e2291b68abf87561a330f0834de0eeb2b024ec2c
MariuszRozycki <mariansc83@icloud.com>
2024-12-23 19:08:58 +0100

    Remove one browser

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
+
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -15,9 +15,7 @@ createRoot(document.getElementById("root")).render(
       <FavoritesProvider>
         <SearchProvider>
           <GoogleReCaptchaProvider reCaptchaKey='6LdjvaMqAAAAALXGgg_2w9B4DpiAnDenvfjXoz-K'>
-            <BrowserRouter basename='/'>
-              <App />
-            </BrowserRouter>
+            <App />
           </GoogleReCaptchaProvider>
         </SearchProvider>
       </FavoritesProvider>
